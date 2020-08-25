9556cfa06ec2da8d0f0581f84ae2300f961aec9a
Jared C. Gober <jcgober@Jareds-Air.lan1>
2020-08-24 20:00:02 -0400

    Added inquirer questions in index.js

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 var inquirer = require("inquirer")
 
+inquirer
+.prompt([
+    {
+        type: "input",
+        message: "What is the name of your project?",
+        name: "project" 
+    },
+    {
+        type: "input",
+        message: "Please Write a description of your application:",
+        name: "description"
+    }
+])
