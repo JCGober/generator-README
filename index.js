9e130817c65ac92389adddbd38e51c14d3e7674b
Jared C. Gober <jcgober@Jareds-Air.lan1>
2020-08-24 20:33:51 -0400

    Added more prompts to index.js

--- index.js
+++ index.js
@@ -11,6 +11,26 @@ inquirer
         type: "input",
         message: "Please Write a description of your application:",
         name: "description"
+    }, 
+    {
+        type:"list",
+        message: "Which license does your project have?",
+        name: "license",
+        choices: [
+            "option1",
+            "option2",
+            "none"
+        ]
+    },
+    {
+        type: "input",
+        message: "What is Your github username?",
+        name: "gitUser"
+    },
+    {
+        type: "input",
+        message: "What is your contact email?",
+        name: "Email"
     }
 ]).then(function(answers){
 
